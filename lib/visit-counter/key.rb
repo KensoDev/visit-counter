0fb08e47268955c935750474c2b4d4db7878f895
Gilad Zohari <gilad@ftbpro.com>
2012-10-08 10:12:23 +0200

    feature - update items regardless of threshold

--- lib/visit-counter/key.rb
+++ lib/visit-counter/key.rb
@@ -1,7 +1,23 @@
 module VisitCounter
   class Key
-    def self.key(object, method)
-      "visit_counter::#{object.class.to_s}::#{object.id}::#{method}"
+
+    attr_reader :object, :method
+    def initialize(object, method)
+      @object = object
+      @method = method
+    end
+
+    def self.generate(method, obj_class, obj_id = nil)
+      ["visit_counter", obj_class, obj_id, method].compact.join("::")
+    end
+
+    def generate_from_instance
+      object_class = self.object_class
+      Key.generate(method,object_class, self.object.id)
+    end
+
+    def object_class
+      object.class.to_s
     end
   end
 end
